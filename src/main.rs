93afbbdca3b791ddf94567c63bf6c26ca88816e5
henry_23 <henry.maoht@outlook.com>
2022-11-08 22:29:31 +0800

    Update "main" on learning "ownership"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hello");
+    s.push_str(" world!");
+    println!("{}", s);
+
+    let t = String::from("12345");
+    let p = f(t);
+    println!("{}", p);
+}
+
+fn f(s: String) -> String {
+    return s;
 }
